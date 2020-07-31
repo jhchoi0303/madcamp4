fcdb73e5f84b68af1722fb1e951046958a3f733e
unknown <zzzmilky@naver.com>
2020-07-31 23:02:24 +0900

    fix link

--- src/component/header/index.tsx
+++ src/component/header/index.tsx
@@ -9,10 +9,10 @@ interface HeaderProps {}
 const Header: React.FC<HeaderProps> = () => {
   return (
     <S.Header>
-      <S.DesktopLink to="/">
+      <S.DesktopLink to="/madcamp4">
         <S.Logo src={STATIC_URL.LOGO} alt="Logo"></S.Logo>
       </S.DesktopLink>
-      <S.MobileLink to="/">
+      <S.MobileLink to="/madcamp4">
         <S.MobileLogo src={STATIC_URL.LOGO_WITHOUT_TEXT} alt="Mobile Logo" />
       </S.MobileLink>
       <S.DesktopButtons>
@@ -20,22 +20,22 @@ const Header: React.FC<HeaderProps> = () => {
           <S.Buttons>
             음악<br></br> 제작소<br></br>(공방)
             <S.DropDown>
-              <S.DesktopLink to="/menu1">
+              <S.DesktopLink to="/madcamp4/menu1">
                 <S.Content>메인</S.Content>
               </S.DesktopLink>
-              <S.DesktopLink to="/menu11">
+              <S.DesktopLink to="/madcamp4/menu11">
                 <S.Content>두번째 공방</S.Content>
               </S.DesktopLink>
             </S.DropDown>
           </S.Buttons>
         </S.Option>
-        <S.DesktopLink to="/menu2">
+        <S.DesktopLink to="/madcamp4/menu2">
           <S.Buttons>앨범</S.Buttons>
         </S.DesktopLink>
-        <S.DesktopLink to="/menu3">
+        <S.DesktopLink to="/madcamp4/menu3">
           <S.Buttons>커뮤너티(전시회)</S.Buttons>
         </S.DesktopLink>
-        <S.DesktopLink to="/menu4">
+        <S.DesktopLink to="/madcamp4/menu4">
           <S.Buttons>내 음악</S.Buttons>
         </S.DesktopLink>
       </S.DesktopButtons>
