8311e0dcb17b1adc8543913dfaa7b5871e1cf43f
unknown <zzzmilky@naver.com>
2020-07-31 22:39:10 +0900

    Updates

---
+++ precache-manifest.c8a15932707266fe4a797e59c76bb647.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4dc970d52273c2cd83a70e8eebae6ce",
+    "url": "/madcamp4/index.html"
+  },
+  {
+    "revision": "5aa68902119aea47de2f",
+    "url": "/madcamp4/static/js/2.234ba8b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/madcamp4/static/js/2.234ba8b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cae7f7f1e03ec5f36a3a",
+    "url": "/madcamp4/static/js/main.22e145b8.chunk.js"
+  },
+  {
+    "revision": "9f56e68d8ca841ea4afe",
+    "url": "/madcamp4/static/js/runtime-main.0b96f2e1.js"
+  }
+]);
